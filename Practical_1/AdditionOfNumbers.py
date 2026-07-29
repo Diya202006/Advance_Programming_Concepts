306f0ea43436170779251cbb41e436f314b17349
Diya202006 <diyakhamkar20@gmail.com>
2026-07-29 21:59:33 +0530

    feat: made changes in the structure , with two folders Practical_1 and Practical_2

---
+++ Practical_1/AdditionOfNumbers.py
@@ -0,0 +1,13 @@
+# Addition of 1 to 10 Numbers using loop
+
+sum = 0
+for i in range(1, 11):
+    sum = sum + i
+
+print("The Addition od 1 to 10 Numbers is(using loop): ", sum)
+
+# Addition of 1 to 10 Numb3ers without using loop 
+
+Addition = 1+2+3+4+5+6+7+8+9+10
+
+print("The Addition od 1 to 10 Numbers is( without using loop): ", Addition)
